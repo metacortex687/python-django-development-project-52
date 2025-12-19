c2f3ea11bf19ee568a0e44f0c8a4cc1cb677304d
metacortex687 <metacortex687@gmail.com>
2025-12-19 22:38:05 +0700

    добавил возможность подключения postgress базы данных из переменных окружения

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -16,6 +16,8 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -85,6 +87,9 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env) #update from DATABASE_URL in .env if defined
 
 
 # Password validation
