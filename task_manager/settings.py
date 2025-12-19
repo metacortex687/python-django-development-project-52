433e98aef421f8c65c3c8ba4177431f6c47f6b30
metacortex687 <metacortex687@gmail.com>
2025-12-20 00:51:03 +0700

    сделал поддержку локализации для английского языка

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -128,3 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+LOCALE_PATHS = [
+    BASE_DIR / "locale",
+]
