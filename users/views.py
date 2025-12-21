fc121543d6e94c7cf896c5daa0b5f7d7285814c8
metacortex687 <metacortex687@gmail.com>
2025-12-21 20:16:33 +0700

    простая форма изменения пользователя

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.views.generic.list import ListView
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 from django.urls import reverse_lazy
 
 class UserListView(ListView):
@@ -12,4 +12,11 @@ class UserListView(ListView):
 class CreateUserView(CreateView):
     template_name = 'user_create.html'
     form_class = UserCreationForm
-    success_url = reverse_lazy('login')    
+    success_url = reverse_lazy('login')   
+
+
+class UpdateUserView(UpdateView):
+    model = User
+    template_name = 'user_update.html'
+    success_url = reverse_lazy('users')
+    fields = ('username', 'first_name', 'last_name')
