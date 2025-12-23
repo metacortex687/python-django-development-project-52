cb71451b89898e14c9bb0e6bc1e0abf65ee05654
metacortex687 <metacortex687@gmail.com>
2025-12-24 01:57:38 +0700

    flash сообщение "Пользователь успешно зарегистрирован"

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -44,7 +44,9 @@ class CreateUserView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login') 
     
-
+    def form_valid(self, form):
+        messages.success(self.request,'Пользователь успешно зарегистрирован')
+        return super().form_valid(form)
 
 class UpdateUserView(UpdateView):
     model = User
