a6ccb784b78138e48b4442e2f1babb9f0a41041e
metacortex687 <metacortex687@gmail.com>
2025-12-24 18:17:34 +0700

    возможность менять пароль в форме обновления пользователя

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,11 +1,13 @@
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.views.generic.list import ListView
 from django.views.generic import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib import messages
 from django import forms
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class CustomUserCreationForm(UserCreationForm):
     first_name = forms.CharField(required=False, label='Имя')
@@ -14,13 +16,33 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         fields=('username','first_name','last_name','password1','password2')  
 
-    # def save(self, commit = ...):
-    #     user = 
-    #     return super().save(commit)
+
+class CustomUserChangeFormForm(UserChangeForm):
+    password = None
+    password1 = forms.CharField(widget=forms.PasswordInput(), label='Пароль', help_text='<ul><li>Ваш пароль должен содержать как минимум 3 символа.</li></ul>')
+    password2 = forms.CharField(widget=forms.PasswordInput(), label='Подтверждение пароля', help_text='Для подтверждения введите, пожалуйста, пароль ещё раз.')   
+
+    class Meta(UserChangeForm.Meta):
+        fields=('username','first_name','last_name',)
+
+    def clean_password1(self):
+        p1 = self.cleaned_data.get('password1')
+        if len(p1) < 3:
+            raise forms.ValidationError('Ваш пароль должен содержать как минимум 3 символа.')
+        return p1
     
+    def clean(self):
+        cleaned = super().clean()
 
+        if cleaned.get('password1') != cleaned.get('password2'):
+            self.add_error('password2','Пароли не совпадают') 
 
-
+        return cleaned  
+    
+    def save(self, commit = ...):
+        self.instance.set_password(self.cleaned_data['password1'])
+        return super().save(commit)
+    
 
 class UserLoginView(LoginView):
     template_name = 'login.html'
@@ -48,14 +70,23 @@ class CreateUserView(CreateView):
         messages.success(self.request,'Пользователь успешно зарегистрирован')
         return super().form_valid(form)
 
-class UpdateUserView(UpdateView):
-    model = User
+
+class UpdateUserView(LoginRequiredMixin, UpdateView):
+    model = get_user_model()
+    form_class = CustomUserChangeFormForm
     template_name = 'user_update.html'
     success_url = reverse_lazy('users')
-    fields = ('username', 'first_name', 'last_name')
+
+    def handle_no_permission(self):
+        messages.error(self.request, 'Вы не авторизованы! Пожалуйста, выполните вход.')
+        return super().handle_no_permission()
 
 
-class DeleteUserView(DeleteView):
+class DeleteUserView(LoginRequiredMixin, DeleteView):
     model = User
     template_name = 'user_delete.html'
     success_url = reverse_lazy('users')
+
+    def handle_no_permission(self):
+        messages.error(self.request, 'Вы не авторизованы! Пожалуйста, выполните вход.')
+        return super().handle_no_permission()
