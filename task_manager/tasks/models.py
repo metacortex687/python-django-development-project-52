8d3b58599715e3139fa584a2eadff66dc62d5207
metacortex687 <metacortex687@gmail.com>
2025-12-24 23:49:13 +0700

    стаутс нельзя удалить если он ссылается на задачи

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -6,7 +6,7 @@ from django.conf import settings
 class Task(models.Model):
     name = models.CharField(max_length=25, verbose_name='Имя')
     description = models.TextField(max_length=25, verbose_name='Описание')
-    status = models.ForeignKey(Status, on_delete=models.SET_NULL, null=True, verbose_name='Статус')
+    status = models.ForeignKey(Status, on_delete=models.RESTRICT, null=True, verbose_name='Статус')
     author = models.ForeignKey(settings.AUTH_USER_MODEL , on_delete=models.RESTRICT, related_name='created_tasks', verbose_name='Автор')
     executor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.RESTRICT, related_name='assigned_tasks', verbose_name='Исполнитель')
     created_at = models.DateTimeField(auto_now_add=True)
